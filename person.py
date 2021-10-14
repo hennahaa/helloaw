42abc5b4a700036d3a8802a29cbd8c050feff0cd
Henna <henna.haapala@gmail.com>
2021-10-14 13:43:59 +0300

    Tehtävä 5.11

--- person.py
+++ person.py
@@ -1,23 +1,34 @@
-class Person():
-    def __init__(self):
-        self.__age = None
-        self.__name = None
+# Tehtävänannon mukainen ihmisen kloonaus
+# https://github.com/academygit/MoreBranchingAndMerging/blob/master/Person.py
+class Person:
+    def __init__(self, city, address, phone_number):
+        self._city = city
+        self._address = address
+        self._phone_number = phone_number
 
     @property
-    def age(self):
-        return self.__age
+    def city(self, _default = None):
+        return self._city
 
-    @age.setter
-    def age(self,ika):
-        self.__age = ika
+    @city.setter
+    def city(self, value):
+        self._city = value
 
     @property
-    def name(self):
-        return self.__name
+    def address(self):
+        return self._address
 
-    @name.setter
-    def name(self,nimi):
-        self.__name = nimi
+    @address.setter
+    def address(self, value):
+        self._address = value
+
+    @property
+    def phone_number(self, _default=None):
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        self._phone_number = value
 
     def __str__(self):
-        return print(f"{self.__name}, aged {self.__age}")
+        return f'Address: {self._address}\nCity: {self._city}\nPhone: {self._phone_number}'
